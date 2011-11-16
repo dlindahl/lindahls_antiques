70411678793c349109a0ff82f901d99bc5d4ed2f
Derek Lindahl <dlindahl@customink.com>
2011-11-15 21:58:51 -0500

    Issue #17 - Added schema.rb, etc.  so that Travis tests will pass.

--- db/development_structure.sql
+++ db/development_structure.sql
@@ -1,10 +1,8 @@
 CREATE TABLE "active_admin_comments" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "resource_id" integer NOT NULL, "resource_type" varchar(255) NOT NULL, "author_id" integer, "author_type" varchar(255), "body" text, "created_at" datetime, "updated_at" datetime, "namespace" varchar(255));
 CREATE TABLE "admin_users" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "email" varchar(255) DEFAULT '' NOT NULL, "encrypted_password" varchar(128) DEFAULT '' NOT NULL, "reset_password_token" varchar(255), "remember_created_at" datetime, "sign_in_count" integer DEFAULT 0, "current_sign_in_at" datetime, "last_sign_in_at" datetime, "current_sign_in_ip" varchar(255), "last_sign_in_ip" varchar(255), "created_at" datetime, "updated_at" datetime);
 CREATE TABLE "antiques" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar(255), "sku" varchar(255), "description" text, "width" float, "height" float, "depth" float, "weight" float, "created_at" datetime, "updated_at" datetime);
-CREATE TABLE "ebay_auction_categories" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar(255), "category_id" integer, "parent_id" integer, "level" integer, "version" integer, "leaf_category" boolean, "created_at" datetime, "updated_at" datetime);
-CREATE TABLE "ebay_auction_categories_ebay_auctions" ("ebay_auction_id" integer, "ebay_auction_category_id" integer);
-CREATE TABLE "ebay_auctions" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "title" varchar(255), "item_number" varchar(255), "listing_status" varchar(255), "time_left" varchar(255), "description" text, "start_price" decimal DEFAULT 0.99, "current_price" decimal DEFAULT 0.99, "reserve_price" decimal, "buy_it_now_price" decimal, "shipping_price" decimal, "antique_id" integer, "primary_category_id" integer, "winner_id" integer, "length" integer, "bids" integer DEFAULT 0, "hit_count" integer DEFAULT 0, "watch_count" integer DEFAULT 0, "reserve_met" boolean DEFAULT 'f', "start_time" datetime, "end_time" datetime, "date_ended" datetime, "created_at" datetime, "updated_at" datetime);
-CREATE TABLE "photos" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "antique_id" integer, "width" integer, "height" integer, "title" varchar(255), "url" varchar(255), "page" varchar(255), "size" varchar(255), "created_at" datetime, "updated_at" datetime);
+CREATE TABLE "ebay_auctions" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "title" varchar(255), "item_number" varchar(255), "listing_status" varchar(255), "time_left" varchar(255), "description" text, "start_price" decimal(7,2) DEFAULT 0.99, "current_price" decimal(7,2) DEFAULT 0.99, "reserve_price" decimal(7,2), "buy_it_now_price" decimal(7,2), "shipping_price" decimal(7,2), "antique_id" integer, "primary_category_id" integer, "winner_id" integer, "length" integer, "bids" integer DEFAULT 0, "hit_count" integer DEFAULT 0, "watch_count" integer DEFAULT 0, "reserve_met" boolean DEFAULT 'f', "start_time" datetime, "end_time" datetime, "date_ended" datetime, "created_at" datetime, "updated_at" datetime);
+CREATE TABLE "photos" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "antique_id" integer, "width" integer, "height" integer, "title" varchar(255), "url" varchar(255), "page" varchar(255), "size" varchar(255), "created_at" datetime, "updated_at" datetime, "flickr_id" integer(8));
 CREATE TABLE "schema_migrations" ("version" varchar(255) NOT NULL);
 CREATE INDEX "index_active_admin_comments_on_author_type_and_author_id" ON "active_admin_comments" ("author_type", "author_id");
 CREATE INDEX "index_active_admin_comments_on_namespace" ON "active_admin_comments" ("namespace");
@@ -22,10 +20,6 @@ INSERT INTO schema_migrations (version) VALUES ('20110405001838');
 
 INSERT INTO schema_migrations (version) VALUES ('20110424190459');
 
-INSERT INTO schema_migrations (version) VALUES ('20110517184123');
-
-INSERT INTO schema_migrations (version) VALUES ('20110517191624');
-
 INSERT INTO schema_migrations (version) VALUES ('20111027144905');
 
 INSERT INTO schema_migrations (version) VALUES ('20111027144906');
@@ -33,3 +27,7 @@ INSERT INTO schema_migrations (version) VALUES ('20111027144906');
 INSERT INTO schema_migrations (version) VALUES ('20111027184900');
 
 INSERT INTO schema_migrations (version) VALUES ('20111028002153');
+
+INSERT INTO schema_migrations (version) VALUES ('20111030211705');
+
+INSERT INTO schema_migrations (version) VALUES ('20111116023132');
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111030211705) do
+ActiveRecord::Schema.define(:version => 20111116023132) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.integer  "resource_id",   :null => false
@@ -92,7 +93,7 @@ ActiveRecord::Schema.define(:version => 20111030211705) do
     t.string   "size"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "flickr_id"
+    t.integer  "flickr_id",  :limit => 8
   end
 
 end
