50164b034e07359715921935723fc648973cd690
Derek Lindahl <dlindahl@customink.com>
2011-11-16 00:09:56 -0500

    Issue #17 - Re-added test notifier back in.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,6 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 
-require 'purdytest'
-
 # In-memory SQLite Rails testing
 # From http://www.osmonov.com/2011/01/in-memory-sqlite-database-for-testing.html
 load_schema = lambda { load "#{Rails.root}/db/schema.rb" }
@@ -10,9 +8,9 @@ silence_stream(STDOUT, &load_schema)
 
 require 'rails/test_help'
 
-# require 'test_notifier/runner/test_unit'
-
-# TestNotifier.default_notifier = :growl
+require 'purdytest'
+require "test_notifier/runner/minitest"
+TestNotifier.default_notifier = :growl
 
 require 'machinist/active_record'
 require File.dirname(__FILE__) + '/blueprints'
